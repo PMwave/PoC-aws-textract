17ed502a2dcb7274a1dd6d27565f245f42643da5
MICHEL Paul <paul.a.michel@wavestone.com>
2022-07-15 12:28:05 +0200

    update

--- OCR.py
+++ OCR.py
@@ -3,25 +3,42 @@ from trp import Document
 
 
 
-
-
-def OCR(s3BucketName, documentName):
-    textract = boto3.client('textract', region_name='eu-west-1')
-
-    response = textract.analyze_document(
-        Document={
-            'S3Object': {
-                'Bucket': s3BucketName,
-                'Name': documentName
-            }
-        },
-        FeatureTypes=["FORMS"])
+def OCR(s3BucketName, documentName, technique = "OCR"):
     
-    doc = Document(response)
+    folder = documentName.split("/")
+    print(folder)
+    textract = boto3.client('textract', region_name='eu-west-1')
+    if technique == "FORM" :
+        response = textract.analyze_document(
+            Document={
+                'S3Object': {
+                    'Bucket': s3BucketName,
+                    'Name': documentName
+                }
+            },
+            FeatureTypes=["FORMS"])
+        
+        doc = Document(response)
 
-    for page in doc.pages:
-    # Print fields
-        print("Fields:")
-        for field in page.form.fields:
-            print("Key: {}, Value: {}".format(field.key, field.value))
-            print(field)
+        for page in doc.pages:
+        # Print fields
+            print("Fields:")
+            for field in page.form.fields:
+                print("Key: {}, Value: {}".format(field.key, field.value))
+                print(field)
+
+    if technique == "OCR" :
+        response = textract.detect_document_text(
+            Document={
+                'S3Object': {
+                    'Bucket': s3BucketName,
+                    'Name': documentName
+                }
+            })
+        
+        doc = Document(response)
+
+        with open('data/scan/'+folder[-2]+'/output_'+folder[-1]+'.txt', 'w') as f:
+            f.write(doc.pages[0].text)
+
+        
--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import boto3
 from trp import Document
 from OCR import OCR
-
+from WER import word_error_rate
 s3BucketName = "ocrbucket379045149424"
 s3client = boto3.client('s3')
 s3resource = boto3.resource('s3')
 
 
 
-OCR(s3BucketName, "data/scan/constat/Scan12.pdf")
+my_bucket = s3resource.Bucket(s3BucketName)
+
+for my_bucket_object in my_bucket.objects.all():
+    print(my_bucket_object.key)
+    path = str(my_bucket_object.key)
+    folder = path.split("/")
+    OCR(s3BucketName, my_bucket_object.key)
+
+    with open('label_'+folder[-2]+'.txt', 'r') as file:
+        data = file.read().replace('\n', ' ')
+
+
+
+    with open('data/scan/'+folder[-2]+'/output_'+folder[-1]+'.txt', 'r') as file:
+        result = file.read().replace('\n', ' ')
+
+    distance, WER, WAcc = word_error_rate(data, result)
+    print(f"Levenstein distance: {distance}")
+    print(f"Word error rate: {WER}%")
+    print(F"Word Accuracy: {WAcc}%")
+    
+    with open('data/scan/'+folder[-2]+'/result_'+folder[-1]+'.txt', 'w') as f2:
+        f2.write(f"Levenstein distance: {distance}")
+        f2.write(f"Word error rate: {WER}%")
+        f2.write(F"Word Accuracy: {WAcc}%")
